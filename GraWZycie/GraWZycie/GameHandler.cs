28285b879fe2c86711c7281956633933e41d49ac
Banasiak <200299@student.pwr.edu.pl>
2015-03-16 07:11:46 +0100

    Gra w zycie - Xaml

---
+++ GraWZycie/GraWZycie/GameHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class GameHandler
+    {
+        int[,] board = new int[12, 12];
+        private MainWindow window;
+
+        public GameHandler(MainWindow wind)
+        {
+            window = wind;
+        }
+
+        public void BeginPlay()
+        {
+            CreateZeroFilledBoard();
+            CreateRandomPopulation();
+        }
+
+        public void Next()
+        {
+            int[,] newBoard = new int[12, 12];
+            for (int i = 1; i <= 10; ++i)
+            {
+                for (int j = 1; j <= 10; ++j)
+                {
+                    int num = CountNeighbours(i, j);
+                    if (board[i, j] == 0 && num == 3) RiseCell(newBoard, i, j);
+                    if (board[i, j] == 1 && (num == 2 || num == 3)) RiseCell(newBoard, i, j);
+                    if (board[i, j] == 1 && (num < 2 || num > 3)) KillCell(newBoard, i, j);
+                }
+            }
+            board = newBoard;
+        }
+
+        private int CountNeighbours(int i, int j)
+        {
+            int count = board[i - 1, j - 1] + board[i, j - 1] + board[i + 1, j - 1];
+            count += board[i - 1, j] + board[i + 1, j];
+            count += board[i - 1, j + 1] + board[i, j + 1] + board[i + 1, j + 1];
+            return count;
+        }
+
+        private void CreateRandomPopulation()
+        {
+            Random rnd = new Random();
+            int pop = rnd.Next(10, 30);
+            for (int i = 0; i < pop; ++i)
+                RiseCell(board, rnd.Next(1, 10), rnd.Next(1, 10));
+        }
+
+        private void CreateZeroFilledBoard()
+        {
+            for (int i = 0; i < 12; ++i)
+            {
+                for (int j = 0; j < 12; ++j)
+                {
+                    board[i, j] = 0;
+                }
+            }
+        }
+
+        private void KillCell(int[,] board, int i, int j)
+        {
+            board[i, j] = 0;
+            window.OnCellDie(i, j);
+        }
+
+        private void RiseCell(int[,] board, int i, int j)
+        {
+            board[i, j] = 1;
+            window.OnCellRise(i, j);
+        }
+    }
+}
---
+++ GraWZycie/GraWZycie/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfLife
+{
+    public partial class MainWindow : Window
+    {
+        Image[,] images;
+        BitmapImage bitmapDead;
+        BitmapImage bitmapAlive;
+        GameHandler handler;
+        Button nextButton;
+
+        public MainWindow()
+        {
+            bitmapDead = new BitmapImage(new Uri("empty.PNG", UriKind.Relative));
+            bitmapAlive = new BitmapImage(new Uri("full.PNG", UriKind.Relative));
+
+            images = new Image[10, 10];
+            InitializeComponent();
+            DrawBoard();
+
+            handler = new GameHandler(this);
+            handler.BeginPlay();
+        }
+
+        private void DrawBoard()
+        {
+            Grid myGrid = new Grid();
+            myGrid.Margin = new Thickness(5);
+            CreateRowAndColumnDefinitions(myGrid);
+            InitZeroFilledBoard(myGrid);
+            CreateButtons(myGrid);
+            Content = myGrid;
+            Show();
+        }
+
+        private void CreateButtons(Grid myGrid)
+        {
+            nextButton = new Button();
+            nextButton.Width = 85;
+            nextButton.Height = 25;
+            nextButton.Content = "Next";
+            nextButton.Background = new SolidColorBrush(Color.FromRgb(33, 33, 33));
+            nextButton.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            nextButton.Click += NextButton_Click;
+            Grid.SetRow(nextButton, 1);
+            Grid.SetColumn(nextButton, 11);
+            myGrid.Children.Add(nextButton);
+
+            Button resetButton = new Button();
+            resetButton.Width = 85;
+            resetButton.Height = 25;
+            resetButton.Content = "Reset";
+            resetButton.Background = new SolidColorBrush(Color.FromRgb(33, 33, 33));
+            resetButton.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            resetButton.Click += ResetButton_Click;
+            Grid.SetRow(resetButton, 2);
+            Grid.SetColumn(resetButton, 11);
+            myGrid.Children.Add(resetButton);
+        }
+
+        private void InitZeroFilledBoard(Grid myGrid)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    images[i, j] = new Image();
+                    images[i, j].Source = bitmapDead;
+                    Grid.SetRow(images[i, j], i);
+                    Grid.SetColumn(images[i, j], j);
+                    myGrid.Children.Add(images[i, j]);
+                }
+            }
+        }
+
+        private void CreateRowAndColumnDefinitions(Grid myGrid)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                var column = new ColumnDefinition();
+                column.Width = new GridLength(40);
+                myGrid.ColumnDefinitions.Add(column);
+
+                var row = new RowDefinition();
+                row.Height = new GridLength(40);
+                myGrid.RowDefinitions.Add(row);
+            }
+
+            var buttonColumn = new ColumnDefinition();
+            buttonColumn.Width = new GridLength(115);
+            myGrid.ColumnDefinitions.Add(buttonColumn);
+        }
+
+        public void OnCellDie(int i, int j)
+        {
+            images[i - 1, j - 1].Source = bitmapDead;
+        }
+
+        public void OnCellRise(int i, int j)
+        {
+            images[i - 1, j - 1].Source = new BitmapImage(new Uri("full.PNG", UriKind.Relative));
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            handler.Next();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 10; ++i)
+                for (int j = 0; j < 10; ++j)
+                    images[i, j].Source = bitmapDead;
+            handler = new GameHandler(this);
+            handler.BeginPlay();
+        }
+    }
+}
